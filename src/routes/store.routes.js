f7ae0db911716ecd380e7f795837b90d23f6fcfb
ecalazans <erickdcalazans@gmail.com>
2025-08-21 01:45:20 -0300

    chore: API - method delete and routes

--- src/routes/store.routes.js
+++ src/routes/store.routes.js
@@ -6,10 +6,11 @@ const storeController = new StoreController()
 
 // Rotas
 const storeRoutes = Router()
-storeRoutes.get("/", storeController.index)
-storeRoutes.get("/:cnpj", storeController.show)
+storeRoutes.get("/all", storeController.index)
+storeRoutes.get("/", storeController.show)
 storeRoutes.post("/", storeController.create)
 storeRoutes.put("/", storeController.update)
+storeRoutes.delete("/", storeController.delete)
 
 
 module.exports = storeRoutes
